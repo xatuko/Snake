b4d3502efba4272c9d1f22101e9163ce5b6ebb1f
Ivan <vin.ven@yandex.ru>
2021-09-24 14:11:35 +0300

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include "snake.hpp"
+
+std::atomic<uint8_t> dir;
+std::atomic<bool> status;
+
+
+void reader()
+{
+    int c;
+    while(status)
+    {
+        c = getc(stdin);
+        switch(c)
+        {
+            case 66:
+                dir = {1};
+                break;
+            case 65:
+                dir = {0};
+                break;
+            case 67:
+                dir = {3};
+                break;
+            case 68:
+                dir = {2};
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+int main(int, char**) {
+    dir = {1};
+    status = {true};
+    Snake kobra;
+    std::thread thr(reader);
+    
+    
+    system("clear");
+    std::vector<std::string> buf;
+    
+    kobra.draw(10,10, buf);
+    printBuf(buf);
+    sleep(1);
+
+    for(int i = 0; i < 20; i++)
+    {
+        
+        system("clear");
+        kobra.setDir(dir);
+        kobra.step();
+        kobra.draw(10,10, buf);
+        printBuf(buf);
+        sleep(1);
+    }
+    status = {false};
+    thr.join();
+    return 0;
+}
